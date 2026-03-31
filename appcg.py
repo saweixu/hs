1213b61669bbf906a75e57457f27f349223ef2be
saweixu <saweixu.athina@gmail.com>
2026-03-31 12:59:33 +0200

    Update appcg.py

--- appcg.py
+++ appcg.py
@@ -10,8 +10,18 @@ from openpyxl.cell.cell import MergedCell
 # =========================================================
 # CONFIG
 # =========================================================
+st.set_page_config(
+    page_title="Athina Logistics Tool",
+    page_icon="logo.png",
+    layout="wide"
+)
+
+st.sidebar.image("logo.png", width=200)
+st.sidebar.markdown("### Athina Logistics")
+st.sidebar.caption("Global Access")
+
 st.set_page_config(page_title="HS Code Checker CG", layout="wide")
-st.title("HS Code Checker")
+st.title("HS Code Checker CG")
 st.caption(
     "Upload one or more invoice files. "
     "The app checks HS codes and flags:"
